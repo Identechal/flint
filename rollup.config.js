21245857dc3b9146ad3f3453c412bda086a3bc45
Jacob Brokaw <jacob@identechal.com>
2022-08-07 16:18:18 -0500

    Refactor folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
+const npmPackage = require(process.cwd() + '/package.json');
+
 export default {
-  input: 'index.js',
+  input: npmPackage.main,
   output: {
-    file: 'bundle.cjs',
+    file: npmPackage.bin,
     format: 'cjs',
   },
   plugins: [nodeResolve({ preferBuiltins: true }), commonJs(), json()],
